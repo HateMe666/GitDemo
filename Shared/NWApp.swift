b4c92be29bceead775f14a2ae819d33d4d9e8463
HateMe666 <nikita.kuzmich.11@gmail.com>
2021-10-27 16:19:28 +0300

    Initial Commit

---
+++ Shared/NWApp.swift
@@ -0,0 +1,17 @@
+//
+//  NWApp.swift
+//  Shared
+//
+//  Created by Nikita Kuzmich on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct NWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
